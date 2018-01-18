a25c5346313ee6b2ed6d7c86ed3eaf9d825f56ff
ShinobiWannabe <34147391+ShinobiWannabe@users.noreply.github.com>
2018-01-17 23:16:52 -0500

    Each file to its own output file
    Right here i much better than what I was doing before.  It now comes out much more nicely formatted.  Now to try and make a csv file for it.

--- FormatLegendOfHeroesScript/FormatFiles.cs
+++ FormatLegendOfHeroesScript/FormatFiles.cs
@@ -8,50 +8,40 @@ namespace FormatLegendOfHeroesScript
     class FormatFiles
     {
 
-
-
         public void Format()
         {
-
             try
             {
-
                 //https://msdn.microsoft.com/en-us/library/system.text.encoding.codepage(v=vs.110).aspx#See Also
                 //https://www.nuget.org/packages/System.Text.Encoding.CodePages/
 
-               // string line;
-                DirectoryInfo di = new DirectoryInfo(@"C: \Users\Michael\Documents\visual studio 2017\Projects\FormatLegendOfHeroesScript\FormatLegendOfHeroesScript\bin\Debug\netcoreapp1.1\");
+                // string line;
+                //DirectoryInfo di = new DirectoryInfo(@"C:\Users\Michael\Documents\visual studio 2017\Projects\FormatLegendOfHeroesScript\FormatLegendOfHeroesScript\bin\Debug\netcoreapp1.1\");
+                DirectoryInfo di = new DirectoryInfo(@".\bin\Debug\netcoreapp1.1\");
 
-                foreach (FileInfo fi in di.EnumerateFiles())
+                foreach (FileInfo fi in di.EnumerateFiles("*.Dat"))
                 {
-                string filename = @"C:\Users\Michael\Documents\visual studio 2017\Projects\FormatLegendOfHeroesScript\FormatLegendOfHeroesScript\bin\Debug\netcoreapp1.1\T_131.DAT";
-                string filenameOut = @"C:\Users\Michael\Documents\visual studio 2017\Projects\FormatLegendOfHeroesScript\FormatLegendOfHeroesScript\bin\Debug\netcoreapp1.1\out\T_131.txt";
-                string replacedByteArray;
-                replacedByteArray = GetModifiedFile(filename);
-                CreateFile(filenameOut, replacedByteArray);
+                    //string filename = @"C:\Users\Michael\Documents\visual studio 2017\Projects\FormatLegendOfHeroesScript\FormatLegendOfHeroesScript\bin\Debug\netcoreapp1.1\T_131.DAT";
+                    //string filenameOut = @".\bin\Debug\netcoreapp1.1\out\T_131.txt";
+                    string filenameOut = fi.DirectoryName + @"\out\" + fi.Name;
+                   DirectoryInfo dirOut = new DirectoryInfo(@".\bin\Debug\netcoreapp1.1\out\");
+
+                    string replacedByteArray;
+                    replacedByteArray = GetModifiedFile(fi);
+                    CreateFile(filenameOut, replacedByteArray);
                 }
-             
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
 
-        private static void CreateFile(string filenameOut, string replacedByteArray)
-        {
-            using (FileStream fs = new FileStream(filenameOut, FileMode.Create))
-            using (StreamWriter sw = new StreamWriter(fs, System.Text.CodePagesEncodingProvider.Instance.GetEncoding(932)))
-            {
-                sw.WriteLine(replacedByteArray);
-            }
-        }
 
-        private static string GetModifiedFile(string filename)
+        private static string GetModifiedFile(FileInfo fileInfo)
         {
             string replacedByteArray;
-            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            using (FileStream fs = new FileStream(fileInfo.FullName, FileMode.Open))
             {
                 using (StreamReader sr = new StreamReader(fs, System.Text.CodePagesEncodingProvider.Instance.GetEncoding(932)))
                 {
@@ -62,14 +52,29 @@ namespace FormatLegendOfHeroesScript
                     string twoLinebreaks = ((char)0x0D).ToString();
                     twoLinebreaks += twoLinebreaks;
 
-                    replacedByteArray = sr.ReadToEnd().Replace((char)0x01, (char)0x0D); //if you try and have 4 hex digits as a character it will not work for shift-jis
+                    replacedByteArray = sr.ReadToEnd();
+
+                    replacedByteArray = replacedByteArray.Replace((char)0x01, (char)0x0D); //if you try and have 4 hex digits as a character it will not work for shift-jis
                     replacedByteArray = replacedByteArray.Replace(((char)0x1E).ToString(), twoLinebreaks);
-                  
+                    //0x1E is a new block of text
+                    //0x01 is a line end
+                    //0x000000ffff00 seems to indicate the end of the text section.
                 }
             }
 
             return replacedByteArray;
         }
+
+        private static void CreateFile(string filenameOut, string replacedByteArray)
+        {
+
+            using (FileStream fs = new FileStream(filenameOut, FileMode.Create))
+            using (StreamWriter sw = new StreamWriter(fs, System.Text.CodePagesEncodingProvider.Instance.GetEncoding(932)))
+            {
+                sw.WriteLine(replacedByteArray);
+            }
+        }
+
 
         //public void WorkingFormat()
         //{
