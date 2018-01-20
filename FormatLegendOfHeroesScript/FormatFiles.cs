c7b1f94f9ad20af2508e774d058de683e9aa7ad2
ShinobiWannabe <34147391+ShinobiWannabe@users.noreply.github.com>
2018-01-19 19:49:59 -0500

    Output to single file
    Output All files to out/output.csv

--- FormatLegendOfHeroesScript/FormatFiles.cs
+++ FormatLegendOfHeroesScript/FormatFiles.cs
@@ -25,14 +25,19 @@ namespace FormatLegendOfHeroesScript
 
                 DirectoryInfo di = new DirectoryInfo(@".\bin\Debug\netcoreapp1.1\");
 
+                string output = "Filename,Speaker,Game_TextBlock,NoSpeaker_TextBlock\n";
+
+                DeleteOutputIfExists(di);
+
                 foreach (FileInfo fi in di.EnumerateFiles("*.Dat"))
                 {
-                    string filenameOut = fi.DirectoryName + @"\out\" + fi.Name.Replace(".DAT", ".csv");
+                    //string filenameOut = fi.DirectoryName + @"\out\" + fi.Name.Replace(".DAT", ".csv");
                     DirectoryInfo dirOut = new DirectoryInfo(@".\bin\Debug\netcoreapp1.1\out\");
 
-                    string replacedByteArray;
-                    replacedByteArray = GetModifiedFile(fi);
-                    CreateFile(filenameOut, replacedByteArray);
+                    //string replacedByteArray;
+                    output += GetModifiedFile(fi);                    
+                    WriteFile(fi.DirectoryName + @"\out\output.csv", output);
+                    output = string.Empty;
                 }
             }
             catch (Exception)
@@ -41,10 +46,22 @@ namespace FormatLegendOfHeroesScript
             }
         }
 
+        private static void DeleteOutputIfExists(DirectoryInfo di)
+        {
+            try
+            {
+                File.Delete(di.FullName + @"\out\output.csv");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("output.csv exists and is probably in use. Canno delete to overwrite.");
+            }
+          
+        }
 
         private static string GetModifiedFile(FileInfo fileInfo)
         {
-            string replacedByteArray = "Filename,Speaker,Game_TextBlock,NoSpeaker_TextBlock\n";
+            string output = string.Empty;
             using (FileStream fs = new FileStream(fileInfo.FullName, FileMode.Open))
             {
                 using (StreamReader sr = new StreamReader(fs, System.Text.CodePagesEncodingProvider.Instance.GetEncoding(932)))
@@ -61,12 +78,12 @@ namespace FormatLegendOfHeroesScript
                             //maybe input a comma on 0x0401. which seems to be after the top of the text block indicating who is speaking. 
                             speakerIndex = line.IndexOf((char)(0x04));
 
-                            replacedByteArray = GetFileName(fileInfo, replacedByteArray);
-                            replacedByteArray = GetSpeaker(replacedByteArray, line, speakerIndex);
-                            replacedByteArray = GetLine(replacedByteArray, line);
-                            replacedByteArray = GetLineWithoutSpeaker(replacedByteArray, line, speakerIndex);
+                            output = GetFileName(fileInfo, output);
+                            output = GetSpeaker(output, line, speakerIndex);
+                            output = GetLine(output, line);
+                            output = GetLineWithoutSpeaker(output, line, speakerIndex);
 
-                            replacedByteArray += NEWLINE;
+                            output += NEWLINE;
 
                             line = string.Empty;
                             speakerIndex = -1;
@@ -76,29 +93,29 @@ namespace FormatLegendOfHeroesScript
                 }
             }
 
-            return replacedByteArray;
+            return output;
         }
 
-        private static string GetFileName(FileInfo fileInfo, string replacedByteArray)
+        private static string GetFileName(FileInfo fileInfo, string output)
         {
-            replacedByteArray += fileInfo.Name;
-            replacedByteArray += COMMA;
-            return replacedByteArray;
+            output += fileInfo.Name;
+            output += COMMA;
+            return output;
         }
 
-        private static string GetSpeaker(string replacedByteArray, string line, int speakerIndex)
+        private static string GetSpeaker(string output, string line, int speakerIndex)
         {
-            replacedByteArray += speakerIndex > -1 ? line.Substring(0, speakerIndex + 1) : string.Empty;
-            replacedByteArray += COMMA;
-            return replacedByteArray;
+            output += speakerIndex > -1 ? line.Substring(0, speakerIndex + 1) : string.Empty;
+            output += COMMA;
+            return output;
         }
 
 
-        private static string GetLine(string replacedByteArray, string line)
+        private static string GetLine(string output, string line)
         {
-            replacedByteArray += line;
-            replacedByteArray += COMMA;
-            return replacedByteArray;
+            output += line;
+            output += COMMA;
+            return output;
         }
 
         private static string GetLineWithoutSpeaker(string replacedByteArray, string line, int speakerIndex)
@@ -108,16 +125,25 @@ namespace FormatLegendOfHeroesScript
             return replacedByteArray;
         }
 
-
-        private static void CreateFile(string filenameOut, string replacedByteArray)
-        {
-
-            using (FileStream fs = new FileStream(filenameOut, FileMode.Create))
+        private static void WriteFile(string filenameOut, string output)
+        {       
+            using (FileStream fs = new FileStream(filenameOut, FileMode.Append))
             using (StreamWriter sw = new StreamWriter(fs, System.Text.CodePagesEncodingProvider.Instance.GetEncoding(932)))
             {
-                sw.WriteLine(replacedByteArray);
+                sw.WriteLine(output);
             }
         }
+
+        //older
+        //private static void CreateFile(string filenameOut, string replacedByteArray)
+        //{
+
+        //    using (FileStream fs = new FileStream(filenameOut, FileMode.Create))
+        //    using (StreamWriter sw = new StreamWriter(fs, System.Text.CodePagesEncodingProvider.Instance.GetEncoding(932)))
+        //    {
+        //        sw.WriteLine(replacedByteArray);
+        //    }
+        //}
 
 
     }
